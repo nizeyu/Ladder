d89a886d10290df98c9cd80204c6edb67012c3f7
Zeyu Ni <zeyuni827@gmail.com>
2017-06-14 10:47:33 -0700

    Update 017.Subsets.java

--- 1.Permutations/017.Subsets.java
+++ 1.Permutations/017.Subsets.java
@@ -5,17 +5,17 @@ class Solution {
      */
     public ArrayList<ArrayList<Integer>> subsets(int[] nums) {
         // write your code here
-        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> results = new ArrayList<ArrayList<Integer>>();
         
         // null
         if(nums == null) {
-            return res;
+            return results;
         }
         
         // []
         if(nums.length == 0) {
-            res.add(new ArrayList<Integer>());
-            return res;// [[]]
+            results.add(new ArrayList<Integer>());
+            return results;// [[]]
         }
         
         Arrays.sort(nums);
@@ -23,22 +23,22 @@ class Solution {
         // [2, 1] == [1, 2]
         // 寻找所有 [] 开头的子集， 放到 res 里
         // 所有的字符串以 "" 开头
-        backtracking(nums, 0, new ArrayList<Integer>(), res);
+        backtracking(nums, 0, new ArrayList<Integer>(), results);
         
-        return res;
+        return results;
     }
     
     //1. 递归的定义：在nums中寻找所有以subset 开头的子集，并放入 res
-    private void backtracking(int[] nums, int offset, ArrayList<Integer> subset, ArrayList<ArrayList<Integer>> res) {
+    private void backtracking(int[] nums, int offset, ArrayList<Integer> subset, ArrayList<ArrayList<Integer>> results) {
         // 2. 递归的拆解
         // 克隆操作 clone / 深度拷贝 deep copy
-        res.add(new ArrayList<Integer>(subset));
+        results.add(new ArrayList<Integer>(subset));
         
-        for(int i =  offset; i < nums.length; ++i) {
+        for(int i =  offset; i < nums.length; i++) {
             subset.add(nums[i]);
             // [] -> [1]
             // 把所有以[1]开头的子集都放到了 res 里
-            backtracking(nums, i + 1, subset, res);
+            backtracking(nums, i + 1, subset, results);
             // backtracking: [3] -> []
             subset.remove(subset.size() - 1);
         }
