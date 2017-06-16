05d7572374db98f7ca9bf46515c2c377608c086d
Zeyu Ni <zeyuni827@gmail.com>
2017-06-15 17:50:58 -0700

    Create 075.Find Bad Version.java

---
+++ 2.Binary Search/075.Find Bad Version.java
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param A: An integers array.
+     * @return: return any of peek positions.
+     */
+    public int findPeak(int[] A) {
+        // write your code here
+        for(int i = 1; i < A.length - 1; i++) {
+            if(A[i-1] < A[i] && A[i] > A[i+1]) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
