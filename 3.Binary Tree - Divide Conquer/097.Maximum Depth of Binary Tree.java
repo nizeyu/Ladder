db14394d111a1fc8d7811b9aff11a5d26abd0f25
Zeyu Ni <zeyuni827@gmail.com>
2017-06-20 12:14:21 -0700

    Update 097.Maximum Depth of Binary Tree.java

--- 3.Binary Tree - Divide Conquer/097.Maximum Depth of Binary Tree.java
+++ 3.Binary Tree - Divide Conquer/097.Maximum Depth of Binary Tree.java
@@ -1,3 +1,4 @@
+// Version 1: Divide Conquer
 /**
  * Definition of TreeNode:
  * public class TreeNode {
@@ -15,7 +16,6 @@ public class Solution {
      * @return: An integer.
      */
     public int maxDepth(TreeNode root) {
-        // write your code here
         if (root == null) {
             return 0;
         }
@@ -24,5 +24,43 @@ public class Solution {
         int rightDepth = maxDepth(root.right);
         
         return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
+
+// version 2: Traverse
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: An integer.
+     */
+    public int depth;
+    public int maxDepth(TreeNode root) {
+        depth = 0;
+        helper(root, 1);
+        return depth;
+    }
+    
+    private void helper(TreeNode node, int curDepth) {
+        if(node == null) {
+            return;
+        }
+        
+        if(curDepth > depth) {
+            depth = curDepth;
+        }
+        
+        helper(node.left, curDepth + 1);
+        helper(node.right, curDepth + 1);
     }
 }
