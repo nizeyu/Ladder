7f99a72439d6ec0cea6cc81369ccbf115791582a
Zeyu Ni <zeyuni827@gmail.com>
2017-06-21 12:18:40 -0700

    Create 069.Binary Tree Level Order Traversal.java

---
+++ 4.Breadth First Search/069.Binary Tree Level Order Traversal.java
@@ -0,0 +1,47 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+ 
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Level order a list of lists of integer
+     */
+    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+        ArrayList<ArrayList<Integer>> results = new ArrayList<>();
+        if(root == null) {
+            return results;
+        }
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        
+        while(!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+            for(int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                level.add(node.val);
+                
+                if(node.left != null) {
+                    q.offer(node.left);
+                }
+                
+                if(node.right != null) {
+                    q.offer(node.right);
+                }
+            }
+            
+            results.add(new ArrayList<>(level));
+        }
+        return results;
+    }
+}
