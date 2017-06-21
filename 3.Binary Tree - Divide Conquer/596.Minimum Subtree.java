68fac096d1ac9de2996b17b56c95d25033ad7568
Zeyu Ni <zeyuni827@gmail.com>
2017-06-20 20:19:16 -0700

    Update 596.Minimum Subtree.java

--- 3.Binary Tree - Divide Conquer/596.Minimum Subtree.java
+++ 3.Binary Tree - Divide Conquer/596.Minimum Subtree.java
@@ -1,3 +1,4 @@
+// version 1 : traverse + divide conquer
 /**
  * Definition of TreeNode:
  * public class TreeNode {
@@ -49,3 +50,69 @@ public class Solution {
         return sum;
     }
 }
+
+// version 2: Pure divide conquer
+* public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+class ResultType {
+    int sum, miniSum;
+    TreeNode miniRoot;
+    ResultType(int sum, int miniSum, TreeNode miniRoot) {
+        this.sum = sum;
+        this.miniSum = miniSum;
+        this.miniRoot = miniRoot;
+    }
+} 
+
+public class Solution {
+    /**
+     * @param root the root of binary tree
+     * @return the root of the minimum subtree
+     */
+    public TreeNode findSubtree(TreeNode root) {
+        if(root == null) {
+            return null;
+        }
+        
+        ResultType res = helper(root);
+        
+        return res.miniRoot;
+    }
+    
+    private ResultType helper(TreeNode node) {
+        if(node == null) {
+            return new ResultType(0, Integer.MAX_VALUE, null);
+        }
+        
+        ResultType left = helper(node.left);
+        ResultType right = helper(node.right);
+        
+        int s = left.sum + right.sum + node.val;
+        int m = Math.min(left.miniSum, right.miniSum);
+        TreeNode miniNode = null;
+        if(left.miniSum < right.miniSum) {
+            miniNode = left.miniRoot;
+        } else {
+            miniNode = right.miniRoot;
+        }
+        
+        if(s < m) {
+            m = s;
+            miniNode = node;
+        }
+        
+        return new ResultType(s, m, miniNode);
+    }
+}
+
+
+
+
+
