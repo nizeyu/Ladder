4816d0200f484a49d003dab8b398fad33b167998
Zeyu Ni <zeyuni827@gmail.com>
2017-06-21 11:08:33 -0700

    Update 088.Lowest Common Ancestor.java

--- 3.Binary Tree - Divide Conquer/088.Lowest Common Ancestor.java
+++ 3.Binary Tree - Divide Conquer/088.Lowest Common Ancestor.java
@@ -1,40 +1,29 @@
-/**
- * Definition of TreeNode:
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left, right;
- *     public TreeNode(int val) {
- *         this.val = val;
- *         this.left = this.right = null;
- *     }
- * }
- */
+//Version 1 : Divide & Conquer
 public class Solution {
-    /**
-     * @param root: The root of the binary search tree.
-     * @param A and B: two nodes in a Binary.
-     * @return: Return the least common ancestor(LCA) of the two nodes.
-     */
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode A, TreeNode B) {
-        // write your code here
-        // write your code here
-        if(root == null || root == A || root == B) {
+    // 在root为根的二叉树中找A,B的LCA:
+    // 如果找到了就返回这个LCA
+    // 如果只碰到A，就返回A
+    // 如果只碰到B，就返回B
+    // 如果都没有，就返回null
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode node1, TreeNode node2) {
+        if (root == null || root == node1 || root == node2) {
             return root;
         }
         
-        TreeNode left = lowestCommonAncestor(root.left, A, B);
-        TreeNode right = lowestCommonAncestor(root.right, A, B);
+        // Divide
+        TreeNode left = lowestCommonAncestor(root.left, node1, node2);
+        TreeNode right = lowestCommonAncestor(root.right, node1, node2);
         
-        if(left != null && right != null) {
+        // Conquer
+        if (left != null && right != null) {
             return root;
-        }
+        } 
         if (left != null) {
             return left;
         }
         if (right != null) {
             return right;
         }
-        
         return null;
     }
 }
