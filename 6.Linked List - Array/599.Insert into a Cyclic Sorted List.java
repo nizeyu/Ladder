314b21ecff37af6ed5a65f251b54b05bd620f023
Zeyu Ni <zeyuni827@gmail.com>
2017-07-04 15:35:33 -0700

    Create 599.Insert into a Cyclic Sorted List.java

---
+++ 6.Linked List - Array/599.Insert into a Cyclic Sorted List.java
@@ -0,0 +1,46 @@
+**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param node a list node in the list
+     * @param x an integer
+     * @return the inserted new list node
+     */
+    public ListNode insert(ListNode node, int x) {
+        //System.out.println(node.next.val);
+        if (node == null) {
+            node = new ListNode(x);
+            node.next = node;
+            return node;
+        }
+        
+        ListNode p = node;
+        while (p.next != node) {
+            if (x >= p.val && x <= p.next.val) {
+                break;
+            }
+            
+            if ((p.val > p.next.val) && (x > p.val || x < p.next.val)) {
+                break;
+            }
+            
+            p = p.next;
+        }
+        
+        ListNode q = p.next;
+        p.next = new ListNode(x);
+        p = p.next;
+        p.next = q;
+        
+        return p;
+    }
+}
