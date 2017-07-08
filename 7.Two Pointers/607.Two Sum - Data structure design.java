79eef3de5929d3d7f7d47925319835462b8be780
Zeyu Ni <zeyuni827@gmail.com>
2017-07-08 09:46:12 -0700

    Create 607.Two Sum - Data structure design.java

---
+++ 7.Two Pointers/607.Two Sum - Data structure design.java
@@ -0,0 +1,36 @@
+public class TwoSum {
+    
+    Map<Integer, Integer> map;
+    
+    public TwoSum() {
+        map = new HashMap<Integer, Integer>();
+    }
+    
+    // Add the number to an internal data structure.
+    public void add(int number) {
+        if (map.containsKey(number)) {
+            map.put(number, map.get(number) + 1);
+        } else {
+            map.put(number, 1);
+        }
+        // map.put(number, getOrDefault(number, 0) + 1);
+    }
+
+    // Find if there exists any pair of numbers which sum is equal to the value.
+    public boolean find(int value) {
+        for (int num : map.keySet()) {
+            int num1 = value - num;
+            if ((num == num1 && map.get(num) > 1) ||
+                (num != num1 && map.containsKey(num1))) {
+                    return true;
+                }
+        }
+        return false;
+    }
+}
+
+
+// Your TwoSum object will be instantiated and called as such:
+// TwoSum twoSum = new TwoSum();
+// twoSum.add(number);
+// twoSum.find(value);
