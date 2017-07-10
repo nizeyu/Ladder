e313311aec0ee40d6b349ee345c4b6cd536faaea
Zeyu Ni <zeyuni827@gmail.com>
2017-07-10 11:01:05 -0700

    Create 608.Two Sum - Input array is sorted.java

---
+++ 7.Two Pointers/608.Two Sum - Input array is sorted.java
@@ -0,0 +1,26 @@
+public class Solution {
+    /*
+     * @param nums an array of Integer
+     * @param target = nums[index1] + nums[index2]
+     * @return [index1 + 1, index2 + 1] (index1 < index2)
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        
+        int i = 0;
+        int j = nums.length - 1;
+        while (i < j) {
+            if (nums[i] + nums[j] == target) {
+                result[0] = i + 1;
+                result[1] = j + 1;
+                return result;
+            } else if (nums[i] + nums[j] < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        
+        return result;
+    }
+}
