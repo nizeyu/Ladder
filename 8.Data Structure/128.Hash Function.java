96741011cb1a395f5831460243f60722ecdae012
Zeyu Ni <zeyuni827@gmail.com>
2017-07-13 12:35:18 -0700

    Create 128.Hash Function.java

---
+++ 8.Data Structure/128.Hash Function.java
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param key: A String you should hash
+     * @param HASH_SIZE: An integer
+     * @return an integer
+     */
+    public int hashCode(char[] key,int HASH_SIZE) {
+        
+        long ans = 0;
+        for (int i = 0; i < key.length; i++) {
+            ans = (ans * 33 + (int)(key[i])) % HASH_SIZE;
+        }
+        
+        return (int)ans;
+    }
+}
