37ccd9123d9400c3d978aa612cd1da8e9e2c4fb1
Zeyu Ni <zeyuni827@gmail.com>
2017-07-25 09:23:57 -0700

    Update 459.Closest Number in Sorted Array.java

--- 2.Binary Search/459.Closest Number in Sorted Array.java
+++ 2.Binary Search/459.Closest Number in Sorted Array.java
@@ -1,3 +1,4 @@
+// Self: lowerBound
 public class Solution {
     /**
      * @param A an integer array sorted in ascending order
@@ -5,41 +6,31 @@ public class Solution {
      * @return an integer
      */
     public int closestNumber(int[] A, int target) {
-        // Write your code here
-        if(A == null || A.length == 0) {
+        if (A == null || A.length == 0) {
             return -1;
         }
         
-        int index = lowerBound(A, target);
-        if(index == 0) {
-            return 0;
-        }
-        
-        if(index == A.length) {
-            return A.length - 1;
-        }
-        
-        if(A[index] == target) {
-            return index;
-        }
-        
-        return (target - A[index-1]) < (A[index] - target) ? index-1 : index;
-    }
-    
-    private int lowerBound(int[] A, int target) {
         int l = 0;
         int r = A.length;
         
-        while(l < r) {
+        while (l < r) {
             int mid = l + (r - l) / 2;
             
-            if(target <= A[mid]) {
+            if (target <= A[mid]) {
                 r = mid;
             } else {
                 l = mid + 1;
             }
         }
         
-        return l;
+        if (l == A.length) {
+            return l - 1;
+        }
+        
+        if (l == 0) {
+            return l;
+        }
+        
+        return A[l] - target < target - A[l-1] ? l : l - 1;
     }
 }
