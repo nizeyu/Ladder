df601a019df87675de56c129d19ea2d517d5c88c
Zeyu Ni <zeyuni827@gmail.com>
2017-08-06 08:46:16 -0700

    Update 544.Top k Largest Numbers.java

--- 8.Data Structure/544.Top k Largest Numbers.java
+++ 8.Data Structure/544.Top k Largest Numbers.java
@@ -1,3 +1,40 @@
+// Self
+class Solution {
+    /*
+     * @param nums an integer array
+     * @param k an integer
+     * @return the top k largest numbers in array
+     */
+    public int[] topk(int[] nums, int k) {
+        int[] result = new int[k];
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(k);
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (pq.size() < k) {
+                pq.offer(nums[i]);
+            } else {
+                if (nums[i] > pq.peek()) {
+                    pq.poll();
+                    pq.offer(nums[i]);
+                }
+            }
+        }
+        
+        int i = result.length - 1;
+        while (!pq.isEmpty()) {
+            result[i] = pq.poll();
+            i--;
+        }
+        
+        return result;
+    }
+};
+
+// maxHeap
 class Solution {
     /*
      * @param nums an integer array
