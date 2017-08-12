548c9e3e60b4a5ff806232d57fff2ece6f31f177
Zeyu Ni <zeyuni827@gmail.com>
2017-08-12 10:46:20 -0700

    Update 611.Knight Shortest Path.java

--- 4.Breadth First Search/611.Knight Shortest Path.java
+++ 4.Breadth First Search/611.Knight Shortest Path.java
@@ -1,7 +1,8 @@
+// Self
 /**
  * Definition for a point.
  * public class Point {
- *     publoc int x, y;
+ *     public int x, y;
  *     public Point() { x = 0; y = 0; }
  *     public Point(int a, int b) { x = a; y = b; }
  * }
@@ -13,51 +14,56 @@ public class Solution {
      * @return the shortest path 
      */
     public int shortestPath(boolean[][] grid, Point source, Point destination) {
-        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+        if (grid == null || grid.length == 0 || source == null || destination == null) {
             return -1;
         }
         
-        int[] deltaX = {1, 1, -1, -1, 2, 2, -2, -2};
-        int[] deltaY = {2, -2, 2, -2, 1, -1, 1, -1};
+        if (grid[source.x][source.y] == true || grid[destination.x][destination.y] == true) {
+            return -1;
+        }
         
-        int distance = 0;
+        int[] directX = {1, 1, -1, -1, 2, 2, -2, -2};
+        int[] directY = {2, -2, 2, -2, 1, -1, 1, -1};
+        
+        int steps = 0;
         Queue<Point> q = new LinkedList<>();
-        
         q.offer(source);
         
         while (!q.isEmpty()) {
+            steps++;
             int size = q.size();
             
             for (int i = 0; i < size; i++) {
                 Point p = q.poll();
                 
-                if (p.x == destination.x && p.y == destination.y) {
-                    return distance;
-                }
-                
                 for (int j = 0; j < 8; j++) {
-                    Point t = new Point(p.x + deltaX[j], p.y + deltaY[j]);
+                    int x = p.x + directX[j];
+                    int y = p.y + directY[j];
                     
-                    if (!inBound(t, grid)) {
+                    if (!inBound(x, y, grid)) {
                         continue;
                     }
                     
-                    if (!grid[t.x][t.y]) {
-                        // mark the point not accessible
-                        grid[t.x][t.y] = true;
-                        q.offer(t);
+                    if (grid[x][y] == true) {
+                        continue;
+                    }
+                    
+                    if (x == destination.x && y == destination.y) {
+                        return steps;
+                    }
+                    
+                    if (grid[x][y] == false) {
+                        grid[x][y] = true;
+                        q.offer(new Point(x, y));
                     }
                 }
             }
-            
-            distance++;
         }
-        
         return -1;
     }
     
-    private boolean inBound(Point p, boolean[][] grid) {
-        if (p.x < 0 || p.x >= grid.length || p.y < 0 || p.y >= grid[0].length) {
+    private boolean inBound(int x, int y, boolean[][] grid) {
+        if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length) {
             return false;
         }
         return true;
