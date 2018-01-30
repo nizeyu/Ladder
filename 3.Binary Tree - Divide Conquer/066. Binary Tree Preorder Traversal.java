7b7f68bd4f88b11a58943fd5b88fd5c8e72d4337
Zeyu Ni <zeyuni827@gmail.com>
2018-01-30 14:48:29 -0800

    Update 066. Binary Tree Preorder Traversal.java

--- 3.Binary Tree - Divide Conquer/066. Binary Tree Preorder Traversal.java
+++ 3.Binary Tree - Divide Conquer/066. Binary Tree Preorder Traversal.java
@@ -21,3 +21,29 @@ public class Solution {
     }
 }
 
+//Version 2: Divide & Conquer
+public class Solution {
+    /*
+     * @param root: A Tree
+     * @return: Preorder in ArrayList which contains node values.
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        // null or leaf
+        if (root == null) {
+            return result;
+        }
+        
+        // Divide
+        List<Integer> left = preorderTraversal(root.left);
+        List<Integer> right = preorderTraversal(root.right);
+        
+        // Conquer
+        result.add(root.val);
+        result.addAll(left);
+        result.addAll(right);
+        
+        return result;
+    }
+}
+
