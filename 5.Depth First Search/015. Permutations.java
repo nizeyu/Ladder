d8cc995a92e6420c2e5c230cbdc9408e9f9370b2
Zeyu Ni <zeyuni827@gmail.com>
2018-03-07 17:04:44 -0800

    Update 015. Permutations.java

--- 5.Depth First Search/015. Permutations.java
+++ 5.Depth First Search/015. Permutations.java
@@ -1,3 +1,46 @@
+#Method 1
+public class Solution {
+    /*
+     * @param nums: A list of integers.
+     * @return: A list of permutations.
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        if (nums == null) {
+            return results;
+        }
+        
+        if (nums.length == 0) {
+            results.add(new ArrayList<>());
+            return results;
+        }
+        
+        dfs(nums, new boolean[nums.length], new ArrayList<>(), results);
+        
+        return results;
+        
+    }
+    
+    private void dfs(int[] nums, boolean[] used, List<Integer> permutation, List<List<Integer>> results) {
+        if (permutation.size() == nums.length) {
+            results.add(new ArrayList<>(permutation));
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            
+            permutation.add(nums[i]);
+            used[i] = true;
+            dfs(nums, used, permutation, results);
+            used[i] = false;
+            permutation.remove(permutation.size() - 1);
+        }
+    }
+}
+
+#Method 2
 public class Solution {
     /*
      * @param nums: A list of integers.
