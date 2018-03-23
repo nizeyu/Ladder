2e720878fb7dc8c1c2786df81b5ecc4a1fa9b6bb
Zeyu Ni <zeyuni827@gmail.com>
2018-03-22 22:56:59 -0700

    Update 013.strStr.java

--- 1.Permutations/013.strStr.java
+++ 1.Permutations/013.strStr.java
@@ -1,3 +1,33 @@
+#Method 1
+public class Solution {
+    /*
+     * @param source: source string to be scanned.
+     * @param target: target string containing the sequence of characters to match
+     * @return: a index to the first occurrence of target in source, or -1  if target is not part of source.
+     */
+    public int strStr(String source, String target) {
+        if (source == null || target == null) {
+            return -1;
+        }
+        
+        for (int i = 0; i < source.length() - target.length() + 1; i++) {
+            int j = 0;
+            for (j = 0; j < target.length(); j++) {
+                if (source.charAt(i + j) != target.charAt(j)) {
+                    break;
+                }
+            }
+            
+            if (j == target.length()) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
+
+
 class Solution {
     /**
      * Returns a index to the first occurrence of target in source,
